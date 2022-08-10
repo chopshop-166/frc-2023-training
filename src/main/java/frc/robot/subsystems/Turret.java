b961badd2737aeba73ff59757221ff5f449b1512
Joe Janicki <joejanicki26@gmail.com>
2022-08-09 20:31:17 -0400

    Limits

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -1,11 +1,9 @@
 package frc.robot.subsystems;
 
 import com.chopshop166.chopshoplib.commands.SmartSubsystemBase;
+import com.chopshop166.chopshoplib.motors.Modifier;
+import com.chopshop166.chopshoplib.motors.ModifierGroup;
 import com.chopshop166.chopshoplib.motors.SmartMotorController;
-
-import org.photonvision.PhotonCamera;
-import org.photonvision.targeting.PhotonPipelineResult;
-import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
@@ -25,19 +23,27 @@ public class Turret extends SmartSubsystemBase {
     private NetworkTableEntry targetEntry = camTable.getEntry("hasTarget");
     private NetworkTableEntry yawEntry = camTable.getEntry("targetYaw");
 
-    private PhotonCamera camera = new PhotonCamera("swerveCam");
-
     private PIDController pid = new PIDController(0.01, 0.0, 0.0);
+
+    private final double ENCODER_MAX = 15.4;
+    private final double ENCODER_MIN = -27.0;
 
     private double ballX = 0.0;
 
+    private ModifierGroup modifiers;
+
     public Turret(TurretMap map) {
         this.motor = map.getMotor();
+
+        this.modifiers = new ModifierGroup(
+                Modifier.upperLimit(() -> motor.getEncoder().getDistance() >= ENCODER_MAX),
+                Modifier.lowerLimit(() -> motor.getEncoder().getDistance() <= ENCODER_MIN));
+
     }
 
     public CommandBase follow() {
         return running("Follow", () -> {
-            motor.set(pid.calculate(ballX));
+            motor.set(modifiers.applyAsDouble(pid.calculate(ballX)));
         });
     }
 
@@ -58,6 +64,8 @@ public class Turret extends SmartSubsystemBase {
         }
         SmartDashboard.putNumber("Target Yaw", ballX);
 
+        SmartDashboard.putNumber("Encoder", motor.getEncoder().getDistance());
+
     }
 
     @Override
@@ -68,5 +76,6 @@ public class Turret extends SmartSubsystemBase {
     @Override
     public void safeState() {
         motor.set(0);
+        motor.getEncoder().reset();
     }
 }
